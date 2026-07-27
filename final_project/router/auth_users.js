921d51e2955bc8aa2ee1a2a7dc1a46d1c74d1353
AhmedHassan-Ahmed <ahmedhassan8754321@gmail.com>
2026-07-27 19:41:28 +0300

    add files

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,58 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  return users.some((user) => user.username === username);
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  return users.some(
+    (user) => user.username === username && user.password === password,
+  );
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const user = req.body.user;
+  if (!authenticatedUser(user.username, user.pass)) {
+    return res.status(401).json({
+      message: "Invalid username or password",
+    });
+  }
+  if (!user) {
+    return res.status(404).json({ message: "Body Empty" });
+  }
+  let accessToken = jwt.sign({ data: req.body.user }, "access", {
+    expiresIn: 60 * 60,
+  });
+  req.session.authorization = { accessToken };
+  return res.status(200).send("User successfully logged in");
+});
+
+regd_users.get("/user/profile", (req, res) => {
+  console.log(req.user);
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.user.data.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({
+      message: "Book not found"
+    });
+  }
+    books[isbn].reviews[username] = review;
+    return res.json({
+    message: "Review added successfully",
+    book: books[isbn]
+  });
 });
 
 module.exports.authenticated = regd_users;
